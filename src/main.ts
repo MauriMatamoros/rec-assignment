35a8f44e6554c66b2ea5528a1e0fea43a8039bcb
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2024-06-28 14:23:40 -0600

    feat: :sparkles: added class validator and validation pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
+import { ValidationPipe } from '@nestjs/common';
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT);
 }
-bootstrap();
+bootstrap()
+  .then(() => console.log(`Listening on port: ${PORT}`))
+  .catch((err) => console.log(err));
